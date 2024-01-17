6808705bfadb343f7ad1a08ae200027b69b5e5f0
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2024-01-17 18:04:51 +0600

    Add initial files

---
+++ script.js
@@ -0,0 +1,62 @@
+/*
+const add = function (...nums) {
+  return nums.reduce((sum, current) => sum + current, 0);
+};
+
+const subtract = function (num1 = 0, num2 = 0) {
+  return num1 - num2;
+};
+
+const sum = function (arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
+};
+
+const multiply = function (arr) {
+  return arr.reduce((sum, current) => sum * current, 1);
+};
+
+const power = function (num1, num2) {
+  return num1 ** num2;
+};
+
+const factorial = function (num) {
+  let result = 1;
+  for (let i = 1; i<=num; i++) {
+    result = result * i;
+  }
+  return result;
+
+
+};
+*/
+
+/* 
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
+};
+
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
+
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
+*/
